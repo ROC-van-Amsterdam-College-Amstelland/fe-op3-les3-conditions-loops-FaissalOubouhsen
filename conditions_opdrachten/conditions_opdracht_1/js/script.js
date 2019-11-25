644cb8e17572d661a2b56a5144c1a10bcae22579
Faissal <2103543@talnet.nl>
2019-11-25 15:24:50 +0100

    opdracht 1

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,16 @@
 //javascript code
+function check() {
+    var saldo = document.getElementById("saldo").value;
+
+    if (saldo<25) {
+        document.getElementById("tekst").innerHTML = "uw saldo is lager dan 25"
+
+    } else {
+        document.getElementById("tekst").innerHTML = "";
+
+
+
+
+    }
+
+}
