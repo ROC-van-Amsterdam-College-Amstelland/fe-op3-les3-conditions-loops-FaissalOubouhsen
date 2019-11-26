476916a4757f125e062171f8346a4f67f9948627
Faissal <2103543@talnet.nl>
2019-11-26 13:10:40 +0100

    opdracht  3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,18 @@
 //javascript code
+function check() {
+    var saldo = document.getElementById("saldo").value;
+
+    if (saldo==500) {
+        document.getElementById("tekst").innerHTML = "uw salaris is gestort!"
+    } else if (saldo>500) {
+            document.getElementById("tekst").innerHTML = "uw salaris met uw bonus is gestort!"
+
+    } else {
+        document.getElementById("tekst").innerHTML = "Er is geen geld gestort!";
+
+
+
+
+    }
+
+}
