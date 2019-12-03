28c5e5fab64f5d7fd9868c7b5d7ae821864fa110
Faissal <2103543@talnet.nl>
2019-12-03 15:27:21 +0100

    opdracht 6

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,25 @@
 //javascript code
+
+function berekenen() {
+    var getal1 = parseInt(document.getElementById("getal1").value);
+    var getal2 = parseInt(document.getElementById("getal2").value);
+    var operator = (document.getElementById("operator").value);
+
+    if (operator == "+") {
+        document.getElementById("antwoord").innerHTML = getal1 + getal2;
+    
+    }
+    if (operator == "-") {
+        document.getElementById("antwoord").innerHTML = getal1 - getal2;
+      
+    }
+    if (operator == "*") {
+        document.getElementById("antwoord").innerHTML = getal1 * getal2;
+      
+    }
+    if (operator == "/") {
+        document.getElementById("antwoord").innerHTML = getal1 / getal2;
+      
+    }
+
+}
